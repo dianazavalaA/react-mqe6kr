ea4ff47cae351c8cdb963280411a573d17eb8e23
dianazavalaA <diana_zavalaa@hotmail.com>
2022-01-29 21:30:18 -0600

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,27 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 
 const App = ()=>{
-  return <h1> <Button /> <Saludo /> <Bunny/> </h1>
+  return <h1> <Saludo />  </h1>
 }
 
-function SaludarEnIdiomas(){
+function SaludarEnIdiomas({idioma}){
   if(idioma === "es") return <p> Hola</p>;
   if(idioma === "es") return <p> Hello</p>;
 }
 
-function Button (){
-  return <p> "es un botón" </p>
-}
-
-const Bunny = ()=>{
-  return <p> "eres el mejor conejo!" </p>
-}
-
-
 const Saludo = () =>{
 const name = 'Diana Laura';
-
-  return <p> Que onda { name } </p>
+  return <p> <SaludarEnIdiomas idioma="es" /> { name } </p>;
 }
 
 render (<App />, document.getElementById('root'))
