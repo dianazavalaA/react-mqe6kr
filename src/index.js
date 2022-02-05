3613c1712aee8fe35ff39e2d0565598aa0030a90
dianazavalaA <diana_zavalaa@hotmail.com>
2022-02-04 22:06:28 -0600

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const Saludo = () =>{
-const name = 'Diana Laura';
-const idioma = "es";
-return(
-    <div>
-     {
-       idioma=== "es"&& <p>Hola</p>
-     }
-    </div>
-)
+const nombres = [
+  'Alvaro',
+  'Diana',
+  'Laura',
+  'Jose'
+]
+
+function getNombres(){
+  const elementosLista = [];
+  for(var i=0;i< nombres.length;i++ ){
+    elementosLista.push(<li>{nombres[i]}</li>);
+  }
+  return elementosLista;
+}
+
+const Nombres = () =>{
+  return <ul> {getNombres()} </ul>
 }
 
 const App = ()=>{
-  return <h1> <Saludo />  </h1>
+  return <h1> <Nombres />  </h1>
 }
 render (<App />, document.getElementById('root'))
