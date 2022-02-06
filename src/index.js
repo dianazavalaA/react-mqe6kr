87db90f63bb65551c9b5f60da698a44a65b69092
dianazavalaA <diana_zavalaa@hotmail.com>
2022-02-05 22:45:30 -0600

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-const App = ()=>{
-  return <h1>Hola Mundo</h1>;
+//const App = ()=>{return <h1>Hola Mundo</h1>;}
+
+class App extends Component{
+  render(){
+    return <h1>Heellooo Peteee</h1>
+  }
 }
 
 
